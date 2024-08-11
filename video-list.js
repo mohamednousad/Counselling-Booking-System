6da04786187c0e2ce6a8ace344cbce14f5366f34
invisbler <119624830+invisbler@users.noreply.github.com>
2024-08-11 22:25:13 +0530

    Add files via upload

---
+++ video-list.js
@@ -0,0 +1,67 @@
+let allVideos = [
+    {
+       name: "Computer Monitor Shape",
+       src: "/video/Mankatha x Rollex",
+       id: "vid_1"
+    },
+    {
+       name: "Button Hover Effect",
+       src: "video/99c78419263c473d95a5fa9190331a30",
+       id: "vid_2"
+    },
+    {
+       name: "Confirm Password using Html CSS & js",
+       src: "/video/3494448aa45f4b16ae447d07484d937e",
+       id: "vid_3"
+    },
+    {
+       name: "Creative Card Hover Effect",
+       src: "video/99c78419263c473d95a5fa9190331a30",
+       id: "vid_4"
+    },
+    {
+       name: "Creative Digital Clock Ui Design",
+       src: "9773df5c5f0ba9cc010e3187b0823b09",
+       id: "vid_5"
+    },
+    {
+       name: "Creative Our Team Section",
+       src: "video/7641d8c1e1784bf68bfc1378e61f4b1c",
+       id: "vid_6"
+    },
+    {
+       name: "Filter Text Animation Using Html and CSS",
+       src: "video/99c78419263c473d95a5fa9190331a30",
+       id: "vid_7"
+    },
+    {
+       name: "Glassmorphism Calculater New Design",
+       src: "./media/Glassmorphism Calculater New Design Html",
+       id: "vid_8"
+    },
+    {
+       name: "How To  Make Cheatsheet",
+       src: "./media/How To Make Cheatsheet Using Html, CSS and Javascript",
+       id: "vid_9"
+    },
+    {
+       name: "How to create Login Form",
+       src: "./media/How to create Login Form Using Html and CSS",
+       id: "vid_10"
+    },
+    {
+       name: "How To Make Animated Menu Icon",
+       src: "./media/How To Make Animated Menu Icon Using Html",
+       id: "vid_11"
+    },
+    {
+       name: "How to make Read More Function",
+       src: "./media/How to make Read More Function using Html",
+       id: "vid_12"
+    },
+    {
+       name: "Vertical Navigation Bar",
+       src: "./media/Vertical Navigation Bar Using Html And CSS",
+       id: "vid_13"
+    }
+ ]
